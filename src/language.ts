c8a2bdb32ac7051b5821431a4c203200a021faf0
VinceJuliano <VinceJuliano>
2022-11-08 11:44:17 -0500

    language help change

--- src/language.ts
+++ src/language.ts
@@ -1,5 +1,5 @@
 import { ARGS } from "./config";
 
 export const LANGUAGE = {
-    invalidArgs: () => (`Invalid args\nRequired args include:\n  create-pool\n  mine <pool-tag>`)
+    invalidArgs: () => (`Invalid args\nRequired args include:\n  create-pool\n  mine <pool-tag> <source(wikipedia/twitter)>`)
 }
