1443099962aa36d76bec31866a9904e708ccbbdb
VinceJuliano <VinceJuliano>
2022-11-21 17:22:37 -0500

    daemon process working

--- build/index.js
+++ build/index.js
@@ -1,59 +1,34 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 exports.__esModule = true;
-var fs_1 = require("fs");
-var path_1 = __importDefault(require("path"));
-var config_1 = require("./config");
-var twitter_1 = require("./artifacts/miners/twitter");
-var wikipedia_1 = require("./artifacts/miners/wikipedia");
-/**
- * Todo: write this cli with named arguments etc...
- */
-var localDir = path_1["default"].join(__dirname, '../local');
-console.log(localDir);
-var POOLS = JSON.parse((0, fs_1.readFileSync)(config_1.POOLS_PATH).toString());
-function create() {
-    console.log(POOLS);
-}
-function mineArtifacts(poolSlug, source, twitterOperation, twitterParam) {
-    if (source === 'twitter') {
-        if (twitterOperation) {
-            if (twitterOperation === 'user') {
-                (0, twitter_1.mineTweetsByUser)(poolSlug, twitterParam);
+var pm2 = require('pm2');
+function processWrapper() {
+    if (process.argv[5] === 'daemon') {
+        pm2.connect(function (err) {
+            if (err) {
+                console.error(err);
+                process.exit(2);
             }
-            else if (twitterOperation === 'mentions') {
-                (0, twitter_1.mineTweetsByMention)(poolSlug, twitterParam);
-            }
-        }
-        else {
-            (0, twitter_1.mineTweets)(poolSlug);
-        }
+            pm2.start({
+                script: 'build/cli.js',
+                name: 'arcpool',
+                args: process.argv
+            }, function (err, apps) {
+                if (err) {
+                    console.error(err);
+                    return pm2.disconnect();
+                }
+                pm2.list(function (err, list) {
+                    // console.log(err, list)
+                    pm2.restart('arcpool', function (err, proc) {
+                        // Disconnects from PM2
+                        pm2.disconnect();
+                    });
+                });
+            });
+        });
     }
-    else if (source === 'wikipedia') {
-        (0, wikipedia_1.mineWikipedia)(poolSlug);
+    else {
+        require('./cli');
     }
-    ;
 }
-switch (process.argv[2]) {
-    case config_1.CLI_ARGS.create:
-        create();
-        break;
-    case config_1.CLI_ARGS.mineArtifacts:
-        if (process.argv[3]) {
-            if (process.argv[4] === 'twitter' || process.argv[4] === 'wikipedia') {
-                mineArtifacts(process.argv[3], process.argv[4], process.argv[5], process.argv[6]);
-            }
-            else {
-                console.log("Invalid args");
-            }
-        }
-        else {
-            console.log("Invalid args");
-        }
-        break;
-    default:
-        console.log("Invalid args");
-        break;
-}
+processWrapper();
--- src/index.ts
+++ src/index.ts
@@ -1,103 +1,41 @@
-import { readFileSync, readdirSync } from "fs";
-import path from 'path';
+
+
 const pm2 = require('pm2');
 
-import { CLI_ARGS, POOLS_PATH } from "./config";
+import { main } from './cli';
 
-import { 
-    mineTweets, 
-    mineTweetsByUser, 
-    mineTweetsByMention 
-} from './artifacts/miners/twitter';
-
-import { mineWikipedia } from './artifacts/miners/wikipedia';
-
-/**
- * Todo: write this cli with named arguments etc...
- */
-
-const localDir = path.join(__dirname, '../local');
-console.log(localDir);
-
-const POOLS = JSON.parse(readFileSync(POOLS_PATH).toString());
-
-function create() {
-    console.log(POOLS);
-}
-
-function mineArtifacts(
-    poolSlug: string, 
-    source: string,
-    twitterOperation: string | null,
-    twitterParam: string | null
-) {
-    if(source ==='twitter') {
-        if(twitterOperation){
-            if(twitterOperation === 'user') {
-                mineTweetsByUser(poolSlug, twitterParam);
-            } else if(twitterOperation ==='mentions') {
-                mineTweetsByMention(poolSlug, twitterParam);
+function processWrapper(){
+    if(process.argv[5] === 'daemon'){
+        pm2.connect(function(err: any) {
+            if (err) {
+              console.error(err)
+              process.exit(2)
             }
-        } else {
-            mineTweets(poolSlug)  
-        }
-    }else if(source === 'wikipedia') {
-        mineWikipedia(poolSlug)
-    };
-}
-
-switch (process.argv[2]) {
-    case CLI_ARGS.create:
-        create();
-        break;
-    case CLI_ARGS.mineArtifacts:
-        if(process.argv[3]){
-            if(process.argv[4] === 'twitter' || process.argv[4] === 'wikipedia'){
-                if(process.argv[5] === 'daemon'){
-                    runPm2();
-                }
-                mineArtifacts(
-                    process.argv[3],
-                    process.argv[4], 
-                    process.argv[5],
-                    process.argv[6]
-                );
-            } else {
-                console.log("Invalid args");
-            }
-        } else {
-            console.log("Invalid args");
-        }
-        break;
-    default:
-        console.log("Invalid args");
-        break;
-}
-
-function runPm2(){
-    pm2.connect(function(err: any) {
-        if (err) {
-          console.error(err)
-          process.exit(2)
-        }
-      
-        pm2.start({
-          script    : 'build/index.js mine crypto-crunch twitter',
-          name      : 'arcpool'
-        }, function(err: any, apps: any) {
-          if (err) {
-            console.error(err)
-            return pm2.disconnect()
-          }
-      
-          pm2.list((err: any, list: any) => {
-            // console.log(err, list)
-      
-            pm2.restart('arcpool', (err: any, proc: any) => {
-              // Disconnects from PM2
-              pm2.disconnect()
+          
+            pm2.start({
+              script    : 'build/cli.js',
+              name      : 'arcpool',
+              args: process.argv
+            }, function(err: any, apps: any) {
+              if (err) {
+                console.error(err)
+                return pm2.disconnect()
+              }
+          
+              pm2.list((err: any, list: any) => {
+                // console.log(err, list)
+          
+                pm2.restart('arcpool', (err: any, proc: any) => {
+                  // Disconnects from PM2
+                  pm2.disconnect()
+                })
+              })
             })
-          })
         })
-    })
+    } else {
+        require('./cli');
+    }
+    
 }
+
+processWrapper();
